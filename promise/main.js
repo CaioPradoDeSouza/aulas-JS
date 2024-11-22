0f7b893dc6b9067a2592f921cc7a0454f96221e7
Caio Prado de Souza <cpssouza7@gmail.com>
2024-11-22 15:18:11 -0300

    feat: update aulas JS

---
+++ promise/main.js
@@ -0,0 +1,39 @@
+const numero = document.getElementById("numero")
+const btn_promessa = document.
+    getElementById("btn_promessa")
+
+btn_promessa.addEventListener("click", (evt) => {
+
+    numero.innerHTML = "Processando..."
+    promessa()
+    .then((retorno) => {
+        numero.innerHTML = retorno
+        numero.classList.remove("erro")
+        numero.classList.add("ok")
+    })
+    .catch((retorno) => {
+        numero.innerHTML = retorno
+        numero.classList.remove("ok")
+        numero.classList.add("erro")
+    })
+})
+
+const promessa = () => {
+    let p = new Promise((resolve, reject) => {
+        let resultado = true
+        let tempo = 3000
+
+        setTimeout(() => {
+            if (resultado) {
+                resolve("Deu tudo certo")
+            } else {
+                reject("Deu tudo errado")
+            }
+        }, tempo)
+
+    })
+
+    return p;
+}
+
+numero.innerHTML = "Esperando..."
