9f74bdde856ad9702ba35675b890ea7085539144
Caio Prado de Souza <cpssouza7@gmail.com>
2024-12-02 17:48:46 -0300

    Feat: Update aulas JS aula criando relógio dispertador

--- date/main.js
+++ date/main.js
@@ -1,6 +1,40 @@
-const data = new Date();
 let divData = document.getElementById('div_data')
 let divRelogio = document.getElementById('div_relogio')
+let timerContent = document.querySelector('.timerContent')
+let inputAlarme = document.getElementById('tmp_alarme')
+let btnAtivar = document.querySelector('.buttonAtivar')
+let btnParar = document.querySelector('.buttonParar')
+let horaAlarme = document.querySelector('#horaDoAlarme')
+
+
+let ts_atual=null
+let ts_alarme=null
+let alarme_ativado=false
+let alarme_tocando=false
+
+btnAtivar.addEventListener('click',()=>{
+
+    ts_atual=Date.now()
+    ts_alarme=ts_atual+(inputAlarme.value*1000)
+    alarme_ativado=true
+    const dt_alarme= new Date(ts_alarme)
+    horaAlarme.innerHTML="Hora do alarme: " + dt_alarme.getHours()+":"+dt_alarme.getMinutes()+":"+dt_alarme.getSeconds()
+    
+
+}
+);
+
+btnParar.addEventListener('click',()=>{
+    
+    alarme_ativado=false
+    horaAlarme.innerHTML="Hora do Alarme"
+    inputAlarme.value=0
+    timerContent.classList.remove("alarme")
+
+}
+);
+
+const data = new Date()
 
 // tratando dia
 
@@ -34,6 +68,14 @@ const relogio=()=>{
     const horaFormatada=hora + `:` + minuto + `:` + segundo
 
     divRelogio.innerHTML = horaFormatada
+
+    if(alarme_ativado){
+        if(data.getTime() >=ts_alarme){
+            
+            timerContent.classList.add("alarme")
+            
+        }
+    }
 
 }
 
