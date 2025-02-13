297da3e6411081592a82b7387652f2309bd41961
Caio Prado de Souza <cpssouza7@gmail.com>
2025-02-13 14:52:19 -0300

    Feat: criado aulas 7 a 9 curso javascript CFB cursos

---
+++ Aula 08/aula08.js
@@ -0,0 +1,11 @@
+// Operadores BitWise 
+
+// let n1 = 13
+// let n2 = 14
+// let res = n1 ^ n2
+
+let n1 = 10
+
+let res = n1 << 2
+
+console.log(res)
---
+++ Aula 09/aula09.js
@@ -0,0 +1,14 @@
+// pré incremento e pós incremento -- o código le primeiro o caractere da esquerda
+
+let n = 10
+
+// incrementa e depois mostra o valor -- serve tambem para as outras operações
+console.log(n)
+console.log(++n)
+
+
+//mostra o valor e depois incrementa -- serve tambem para as outras operações
+
+
+console.log(n++)
+console.log(n)
