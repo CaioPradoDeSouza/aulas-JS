4c5d241318f11bf240218b5ea9947630986731c9
Caio Prado <cpssouza7@gmail.com>
2025-05-02 13:07:32 -0300

    Feat: criado aula 25 curso Javascript

---
+++ Aula 25/aula25.js
@@ -0,0 +1,12 @@
+// ARROW Functions
+
+const soma=(v1,v2)=>{
+    return v1+v2
+}
+console.log(soma(1,3))
+
+const nome=n=>{return n}
+console.log(nome("caio"))
+
+const add=n=>n+10
+console.log(add(10))
