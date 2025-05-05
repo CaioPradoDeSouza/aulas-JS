768bc5d6a48792b9c2c0349062a1d16392b5ec64
Caio Prado <cpssouza7@gmail.com>
2025-05-05 14:20:09 -0300

    Feat: criado aula 27 curso javascript cfb cursos

---
+++ Aula 26/aula26.js
@@ -0,0 +1,14 @@
+// ARROW function aninhada parametrizada com REST e com SPREAD
+
+const soma=(...valores)=>{
+    const somar=val=>{
+        let res=0
+        for(v of val){
+            res+=v
+        }
+        return res
+    }
+    return somar(valores)
+}
+
+console.log(soma(5,5,5))
