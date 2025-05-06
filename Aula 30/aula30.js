2258585bc4b3f51b6ec0a568c753407b8fc76235
Caio Prado <cpssouza7@gmail.com>
2025-05-06 14:33:50 -0300

    Feat: criado aula 30 curso javascript CFB CURSOS

---
+++ Aula 30/aula30.js
@@ -0,0 +1,16 @@
+const c1=document.getElementById("c1")
+const c2=document.getElementById("c2")
+const c3=document.getElementById("c3")
+const c4=document.getElementById("c4")
+const c5=document.getElementById("c5")
+const c6=document.getElementById("c6")
+
+const arraEle=[c1,c2,c3,c4,c5,c6]
+
+// for(e of arraEle){
+//     e.innerHTML="CURSOS"
+// }
+arraEle.map((e)=>{
+    console.log(e)
+})
+
